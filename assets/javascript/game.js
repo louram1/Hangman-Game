5c8feaa08a56e397b4ac0018d4c0035becf66172
Vishy Ram <louram169@gmail.com>
2017-11-19 12:49:00 -0800

    HW commit 1

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+<!-- hide script from old browsers
+
+var i = 0;
+
+
+var cars = ["Saab", "Volvo", "BMW", "Toyota", "Lexus", "Chevrolet", "Chrysler", "Mercedes"];
+function GenerateWord()
+{
+	var GuessWord = "";
+	document.getElementById("demo").innerHTML = "Hello World";
+	console.log("Inside Function");
+
+
+}
+
+// end hiding script from old browsers -->
